ec69c93803e0194e56c568a60f7a3e79a22faaf5
valvalov22 <vlrik@mail.ru>
2022-07-10 17:47:01 +0400

    final version

---
+++ js/script.js
@@ -0,0 +1,31 @@
+import  tabs from './modules/tabs';
+import  modal from './modules/modal';
+import  timer from './modules/timer';
+import  cards from './modules/cards';
+import  calculator from './modules/calculator';
+import  forms from './modules/forms';
+import  slider from './modules/slider';
+import {openModal} from './modules/modal';
+
+window.addEventListener('DOMContentLoaded', function() {
+
+    const modalTimerId = setTimeout(() => openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-12-31');
+    cards();
+    calculator();
+    forms('form', modalTimerId);
+    slider({
+        container: '.offer__slider',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner',
+    });
+});
+
+
